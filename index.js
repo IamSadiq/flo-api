fbcd97edcf6406bc8e29ca3e4f1f72c870135db0
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-02 13:06:32 +0100

    modified index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 mongoose.connect('mongodb://localhost/flowApp');
 const app = express();
 const port = 3000;
+const router = express.Router();
 
 const ProjectCtrl = require('./routes/projects/projects-ctrl');
 const PfiCtrl = require('./routes/pfi/pfi-ctrl');
@@ -23,6 +24,14 @@ app.use(cors()); // enable cross-origin resource sharing
 // app.use('/api/ccvo', CcvoCtrl);
 // app.use('/api/bol', BolCtrl);
 // app.use('/api/eform', EformCtrl);
+
+// test to render at '/'
+router.get('/', (req, res)=>{
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hola! Flo Api");
+    res.end();
+});
+app.use('/', router);
 
 app.listen(port);
 console.log('Node server started on port: '+ port);
