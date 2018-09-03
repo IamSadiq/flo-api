d422f0a97e75a58a08ee57cbb25e5b88cc34e6a9
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-03 09:45:26 +0100

    modified cria-ctrl file

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const PfiCtrl = require('./routes/pfi/pfi-ctrl');
 const EformCtrl = require('./routes/e-form/eform-ctrl');
 const BolCtrl = require('./routes/bill-of-lading/bol-ctrl');
 const CcvoCtrl = require('./routes/ccvo/ccvo-ctrl');
+const CriaCtrl = require('./routes/cria/cria-ctrl');
 
 // middlewares
 app.use(bodyParser.json());
@@ -24,6 +25,7 @@ app.use('/api/pfi', PfiCtrl);
 app.use('/api/ccvo', CcvoCtrl);
 app.use('/api/bol', BolCtrl);
 app.use('/api/eform', EformCtrl);
+app.use('/api/cria', CriaCtrl);
 
 // test to render at '/'
 // router.get('/', (req, res)=>{
