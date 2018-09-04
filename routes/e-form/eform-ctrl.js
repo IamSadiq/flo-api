5c37dca8d2569a8527ead8bba8eb4060278318da
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-04 01:38:48 +0100

    modified eform

--- routes/e-form/eform-ctrl.js
+++ routes/e-form/eform-ctrl.js
@@ -7,22 +7,82 @@ const router = express.Router();
 
 router
 
+// retrieve all collection
 .get('/', (req, res) => {
     Eform.find({}, (err, eforms)=>{
-        if(err) throw err;
-        res.json(eforms);
+        if(err)
+            res.json({status: "failure"});
+        else
+            res.json(eforms);
     });
 })
 
+// retrieve a collection
 .get('/:id', (req, res) => {
     Eform.findById({_id: req.params.id}, (err, eform) => {
-        if(err) throw err;
-        res.json(eform);
+        if(err)
+            res.json({status: "failure"});
+        else
+            res.json(eform);
     });
 })
 
+// insert a collection
 .post('/', (req, res) => {
+    const newEform = Eform({
+        project: req.body.projectId,
+        docFile: req.body.file,
+        productType: req.body.productType,
+        brandName: req.body.brandName,
+        mName: req.body.mName,
+        quantity: req.body.quantity,
+        description: req.body.description,
+        referenceNumber: req.body.referenceNumber,
+        packaging: req.body.packaging
+    });
 
+    newEform.save((err)=>{
+        if(err)
+            res.json({status: "failure"});
+        else
+            res.json({status: "success"});
+    });
+})
+
+// update a collection
+.patch('/:id', (req, res) => {
+    myData = {
+        docFile: req.body.file,
+        productType: req.body.productType,
+        brandName: req.body.brandName,
+        mName: req.body.mName,
+        quantity: req.body.quantity,
+        description: req.body.description,
+        referenceNumber: req.body.referenceNumber,
+        packaging: req.body.packaging
+    };
+
+    Eform.update({project: req.params.id}, myData, (err) => {
+        if(err)
+            res.json({status: "failure"});
+        else
+            res.json({status: "success"});
+    });
+})
+
+// remove a collection
+.delete('/:id', (req, res) => {
+    Eform.remove({project: req.params.id}, (err) => {
+        if(err)
+            res.json({status: "failure"});
+        else
+            res.json({status: "success"});
+    });
 });
+
+// I don't think deleting all db entries is a feature we'll need --- but change my mind, convince me otherwise
+// .delete('/', (req, res) => {
+
+// });
 
 module.exports = router;
--- routes/e-form/eform-model.js
+++ routes/e-form/eform-model.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var EformSchema = new Schema({
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: 'Projects'
+    },
+    docFile: String,
     productType: String,
     brandName: String,
     mName: String,
@@ -9,7 +14,6 @@ var EformSchema = new Schema({
     description: String,
     referenceNumber: String,
     packaging: String
-    // file: File
 });
 
 module.exports = mongoose.model('Eform', EformSchema);
