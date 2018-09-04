62cfa8c1e7adbbfd4327427e0db9b7c6e3d45001
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-04 01:27:37 +0100

    modified invoice

--- routes/invoice/invoice-ctrl.js
+++ routes/invoice/invoice-ctrl.js
@@ -7,32 +7,74 @@ const router = express.Router();
 
 router
 
+// retrieve collections
 .get('/', (req, res) => {
-    Invoice.find({}, (err, pfis)=>{
-        if(err) throw err;
-        res.json(pfis);
+    Invoice.find({}, (err, data)=>{
+        if(err)
+            res.json({status: "failure"});
+        else
+            res.json(data);
     });
 })
 
+// retrieve a collection
 .get('/:id', (req, res) => {
-    Invoice.findById({_id: req.params.id}, (err, pfi) => {
-        if(err) throw err;
-        res.json(pfi);
+    Invoice.findById({_id: req.params.id}, (err, data) => {
+        if(err)
+            res.json({status: "failure"});
+        else
+            res.json(data);
     });
 })
 
+// insert a collection
 .post('/', (req, res) => {
     const newInvoice = Invoice({
-        // file: req.body.file
+        project: req.body.projectId,
+        docFile: req.body.file,
         invoiceNumber: req.body.invoiceNumber,
         quantity: req.body.quantity,
         price: req.body.price
     });
 
     newInvoice.save((err)=>{
-        if(err) return res.status(500).send("failed to save");
-        else return res.status(200).send("successfully saved");
+        if(err)
+            res.json({status: "failure"});
+        else
+            res.json({status: "success"});
+    });
+})
+
+// update a collection
+.patch('/:id', (req, res) => {
+    myData = {
+        docFile: req.body.file,
+        invoiceNumber: req.body.invoiceNumber,
+        quantity: req.body.quantity,
+        price: req.body.price
+    };
+
+    Invoice.update({project: req.params.id}, myData, (err) => {
+        if(err)
+            res.json({status: "failure"});
+        else
+            res.json({status: "success"});
+    });
+})
+
+// remove a collection
+.delete('/:id', (req, res) => {
+    Invoice.remove({project: req.params.id}, (err) => {
+        if(err)
+            res.json({status: "failure"});
+        else
+            res.json({status: "success"});
     });
 });
+
+// I don't think deleting all db entries is a feature we'll need --- but change my mind, convince me otherwise
+// .delete('/', (req, res) => {
+
+// });
 
 module.exports = router;
--- routes/invoice/invoice-model.js
+++ routes/invoice/invoice-model.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var InvoiceSchema = new Schema({
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: 'Projects'
+    },
     invoiceNumber: String,
     quantity: Number,
-    price: Number
-    // file: File
+    price: Number,
+    docFile: String
 });
 
 module.exports = mongoose.model('Invoice', InvoiceSchema);
