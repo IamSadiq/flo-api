b06b4ffc22a24102a9722635c32f584edae8fc77
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-24 14:38:28 +0100

    modified eform

--- routes/e-form/eform-ctrl.js
+++ routes/e-form/eform-ctrl.js
@@ -1,59 +1,84 @@
 const express = require('express');
 const Eform = require('./eform-model');
 
+const VerifyToken = require('../../auth/VerifyToken');
+const User = require('../users/user-model');
 const router = express.Router();
 
 router
 
 // retrieve all collection
-.get('/', (req, res) => {
-    Eform.find({}, (err, eforms)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(eforms);
+.get('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        Eform.find({}, (err, eforms)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(eforms);
+        });
     });
 })
 
 // retrieve a collection
-.get('/:id', (req, res) => {
-    Eform.findById({_id: req.params.id}, (err, eform) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(eform);
+.get('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        Eform.findById({_id: req.params.id}, (err, eform) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(eform);
+        });
     });
 })
 
 // insert a collection
-.post('/', (req, res) => {
+.post('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    Eform.create(req.body, (err, response)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success", eformId: response._id});
+        Eform.create(req.body, (err, response)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success", eformId: response._id});
+        });
     });
 })
 
 // update a collection
-.patch('/:id', (req, res) => {
+.patch('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    Eform.update({_id: req.params.id}, req.body, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+        Eform.update({_id: req.params.id}, req.body, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 })
 
 // remove a collection
-.delete('/:id', (req, res) => {
-    Eform.remove({_id: req.params.id}, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+.delete('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        Eform.remove({_id: req.params.id}, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 });
 
