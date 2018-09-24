e3d2425979c78d9576d420672be88269677a4e7f
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-24 14:49:55 +0100

    modified ccvo

--- routes/ccvo/ccvo-ctrl.js
+++ routes/ccvo/ccvo-ctrl.js
@@ -1,59 +1,84 @@
 const express = require('express');
 const CCVO = require('./ccvo-model');
 
+const VerifyToken = require('../../auth/VerifyToken');
+const User = require('../users/user-model');
 const router = express.Router();
 
 router
 
 // retrieve all collection
-.get('/', (req, res) => {
-    CCVO.find({}, (err, data)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(data);
+.get('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        CCVO.find({}, (err, data)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(data);
+        });
     });
 })
 
 // retrieve a collection
-.get('/:id', (req, res) => {
-    CCVO.findById({_id: req.params.id}, (err, data) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(data);
+.get('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        CCVO.findById({_id: req.params.id}, (err, data) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(data);
+        });
     });
 })
 
 // insert a collection
-.post('/', (req, res) => {
+.post('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    CCVO.create(req.body, (err, response)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success", ccvoId: response._id});
+        CCVO.create(req.body, (err, response)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success", ccvoId: response._id});
+        });
     });
 })
 
 // update a collection
-.patch('/:id', (req, res) => {
+.patch('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    CCVO.update({_id: req.params.id}, req.body, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+        CCVO.update({_id: req.params.id}, req.body, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 })
 
 // remove a collection
-.delete('/:id', (req, res) => {
-    CCVO.remove({_id: req.params.id}, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+.delete('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        CCVO.remove({_id: req.params.id}, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 });
 
