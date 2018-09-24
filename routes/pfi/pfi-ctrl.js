f80dcac37db6a0ac4d662b6cf96cc0e050fb39b8
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-24 13:33:57 +0100

    modified pfi

--- routes/pfi/pfi-ctrl.js
+++ routes/pfi/pfi-ctrl.js
@@ -1,59 +1,84 @@
 const express = require('express');
 const ProFormalInvoice = require('./pfi-model');
 
+const VerifyToken = require('../../auth/VerifyToken');
+const User = require('../users/user-model');
 const router = express.Router();
 
 router
 
 // retrieve all collections
-.get('/', (req, res) => {
-    ProFormalInvoice.find({}, (err, pfis)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(pfis);
+.get('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        ProFormalInvoice.find({}, (err, pfis)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(pfis);
+        });
     });
 })
 
 // retreive a collection
-.get('/:id', (req, res) => {
-    ProFormalInvoice.findById({_id: req.params.id}, (err, pfi) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(pfi);
+.get('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        ProFormalInvoice.findById({_id: req.params.id}, (err, pfi) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(pfi);
+        });
     });
 })
 
 // insert a collection
-.post('/', (req, res) => {
+.post('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    ProFormalInvoice.create(req.body, (err)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+        ProFormalInvoice.create(req.body, (err)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 })
 
 // update a collection
-.patch('/:id', (req, res) => {
+.patch('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    ProFormalInvoice.update({_id: req.params.id}, req.body, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+        ProFormalInvoice.update({_id: req.params.id}, req.body, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 })
 
 // delete a collection
 .delete('/:id', (req, res) => {
-    ProFormalInvoice.remove({project: req.params.id}, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        ProFormalInvoice.remove({project: req.params.id}, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 });
 
