73e2c61df56b0272e333b12c684fa758b1472d55
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-24 14:35:06 +0100

    modified insurance

--- routes/insurance/insurance-ctrl.js
+++ routes/insurance/insurance-ctrl.js
@@ -1,58 +1,84 @@
 const express = require('express');
 const Insurance = require('./insurance-model');
+
+const VerifyToken = require('../../auth/VerifyToken');
+const User = require('../users/user-model');
 const router = express.Router();
 
 router
 
 // retrieve all collections
-.get('/', (req, res) => {
-    Insurance.find({}, (err, data)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(data);
+.get('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        Insurance.find({}, (err, data)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(data);
+        });
     });
 })
 
 // retrieve a collection
-.get('/:id', (req, res) => {
-    Insurance.findById({_id: req.params.id}, (err, data) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(data);
+.get('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        Insurance.findById({_id: req.params.id}, (err, data) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(data);
+        });
     });
 })
 
 // insert a collection
-.post('/', (req, res) => {
+.post('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    Insurance.create(req.body, (err, response)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success", insuranceId: response._id});
+        Insurance.create(req.body, (err, response)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success", insuranceId: response._id});
+        });
     });
 })
 
 // update a collection
-.patch('/:id', (req, res) => {
+.patch('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    Insurance.update({_id: req.params.id}, req.body, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+        Insurance.update({_id: req.params.id}, req.body, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 })
 
 // remove a collection
-.delete('/:id', (req, res) => {
-    Insurance.remove({_id: req.params.id}, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+.delete('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        Insurance.remove({_id: req.params.id}, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 });
 
