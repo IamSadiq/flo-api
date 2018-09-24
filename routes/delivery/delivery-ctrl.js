72c335acdcac18aaf593426c480865e9ecbcb23f
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-24 14:42:08 +0100

    modified delivery

--- routes/delivery/delivery-ctrl.js
+++ routes/delivery/delivery-ctrl.js
@@ -1,59 +1,84 @@
 const express = require('express');
 const Delivery = require('./delivery-model');
 
+const VerifyToken = require('../../auth/VerifyToken');
+const User = require('../users/user-model');
 const router = express.Router();
 
 router
 
 // retrieve all collection
-.get('/', (req, res) => {
-    Delivery.find({}, (err, data)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(data);
+.get('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        Delivery.find({}, (err, data)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(data);
+        });
     });
 })
 
 // retrieve a collection
-.get('/:id', (req, res) => {
-    Delivery.findById({project: req.params.id}, (err, data) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(data);
+.get('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        Delivery.findById({project: req.params.id}, (err, data) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(data);
+        });
     });
 })
 
 // insert a collection
-.post('/', (req, res) => {
+.post('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    Delivery.create(req.body, (err, response)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success", deliveryId: response._id});
+        Delivery.create(req.body, (err, response)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success", deliveryId: response._id});
+        });
     });
 })
 
 // update a collection
-.patch('/:id', (req, res) => {
+.patch('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    Delivery.update({_id: req.params.id}, req.body, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+        Delivery.update({_id: req.params.id}, req.body, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 })
 
 // remove a collection
-.delete('/:id', (req, res) => {
-    Delivery.remove({_id: req.params.id}, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+.delete('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        Delivery.remove({_id: req.params.id}, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 });
 
