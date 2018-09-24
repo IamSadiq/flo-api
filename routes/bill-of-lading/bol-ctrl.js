2fe3b7089bbfdb62a8fc308423bb4172de5d547d
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-24 14:55:42 +0100

    modified bill-of-lading

--- routes/bill-of-lading/bol-ctrl.js
+++ routes/bill-of-lading/bol-ctrl.js
@@ -1,58 +1,83 @@
 const express = require('express');
 const BillOfLading = require('./bol-model');
 
+const VerifyToken = require('../../auth/VerifyToken');
+const User = require('../users/user-model');
 const router = express.Router();
 
 router
 
 // retrieve all collection
-.get('/', (req, res) => {
-    BillOfLading.find({}, (err, data)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(data);
+.get('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        BillOfLading.find({}, (err, data)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(data);
+        });
     });
 })
 
 // retrieve a collection
-.get('/:id', (req, res) => {
-    BillOfLading.findById({_id: req.params.id}, (err, data) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(data);
+.get('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        BillOfLading.findById({_id: req.params.id}, (err, data) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(data);
+        });
     });
 })
 
-.post('/', (req, res) => {
+.post('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    BillOfLading.create(req.body, (err, response) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success", bolID: response._id});
+        BillOfLading.create(req.body, (err, response) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success", bolID: response._id});
+        });
     });
 })
 
 // update a collection
-.patch('/:id', (req, res) => {
+.patch('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    BillOfLading.update({_id: req.params.id}, req.body, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+        BillOfLading.update({_id: req.params.id}, req.body, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 })
 
 // remove a collection
-.delete('/:id', (req, res) => {
-    BillOfLading.remove({_id: req.params.id}, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+.delete('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        BillOfLading.remove({_id: req.params.id}, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 });
 
