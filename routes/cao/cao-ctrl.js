cb906f9c62582b7535abbf4446a40030e070e635
Abu-Bakr Adam <asiddik5@gmail.com>
2018-09-24 14:52:49 +0100

    modified cao

--- routes/cao/cao-ctrl.js
+++ routes/cao/cao-ctrl.js
@@ -1,58 +1,83 @@
 const express = require('express');
 const CAO = require('./cao-model');
 
+const VerifyToken = require('../../auth/VerifyToken');
+const User = require('../users/user-model');
 const router = express.Router();
 
 router
 
 // retrieve all collection
-.get('/', (req, res) => {
-    CAO.find({}, (err, data)=>{
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(data);
+.get('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        CAO.find({}, (err, data)=>{
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(data);
+        });
     });
 })
 
 // retrieve a collection
-.get('/:id', (req, res) => {
-    CAO.findById({_id: req.params.id}, (err, data) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json(data);
+.get('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        CAO.findById({_id: req.params.id}, (err, data) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json(data);
+        });
     });
 })
 
-.post('/', (req, res) => {
+.post('/', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    CAO.create(req.body, (err, response) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success", caoId: response._id});
+        CAO.create(req.body, (err, response) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success", caoId: response._id});
+        });
     });
 })
 
 // update a collection
-.patch('/:id', (req, res) => {
+.patch('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
 
-    CAO.update({_id: req.params.id}, req.body, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+        CAO.update({_id: req.params.id}, req.body, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 })
 
 // remove a collection
-.delete('/:id', (req, res) => {
-    CAO.remove({_id: req.params.id}, (err) => {
-        if(err)
-            res.json({status: "failure"});
-        else
-            res.json({status: "success"});
+.delete('/:id', VerifyToken, (req, res) => {
+    User.findById(req.userId, {password: 0}, (err, user) => {
+        if (err) return res.status(500).send("There was a problem finding the user.");
+        if (!user) return res.status(404).send("No user found.");
+
+        CAO.remove({_id: req.params.id}, (err) => {
+            if(err)
+                res.json({status: "failure"});
+            else
+                res.json({status: "success"});
+        });
     });
 });
 
